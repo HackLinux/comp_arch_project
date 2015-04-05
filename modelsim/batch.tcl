6900913b03f17e6523884b5c9235b7732c58fc81
Loren Lugosch <loren.lugosch@gmail.com>
2015-04-05 14:57:29 -0400

    test of calling tcl from other tcl

--- modelsim/batch.tcl
+++ modelsim/batch.tcl
@@ -14,15 +14,23 @@ proc c  {} {
 }
  
 proc r  {} {    
+    # open simulation
     echo "---------------STARTED RUNNING---------------------------"
     vsim work.memory_tb
     add wave *
     restart
+    
+    # add a breakpoint to the line which contains
+    # the stopping point
     set fp [open "../vhdl/testbench/memory_tb.vhd" r]
     set filecontent [read $fp]
     set input_list [split $filecontent "\n"]
-    lsearch input_list 
-    bp ../vhdl/testbench/memory_tb.vhd 51
+    set stopping_point "rst <=*"
+    set line [lsearch -regexp $input_list $stopping_point]
+    set line [expr $line + 1]
+    bp ../vhdl/testbench/memory_tb.vhd $line
+    
+    # start the simulation
     run -all
     echo "---------------FINISHED RUNNING--------------------------"
 }
@@ -33,11 +41,7 @@ proc q  {} {
 proc Q  {} {
  quit -force
 }
- 
-set fp [open "../vhdl/testbench/memory_tb.vhd" r]
-set filecontent [read $fp]
-set input_list [split $filecontent "\n"]
-set stopping_point "rst <=*"
-set line [lsearch -regexp $input_list $stopping_point]
-set line [expr $line + 1]
-echo $line
+
+#comment?
+c
+r
---
+++ modelsim/compile.tcl
@@ -0,0 +1,1 @@
+source batch.tcl
